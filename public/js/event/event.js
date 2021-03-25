e9ded77cfbf2f39e79a939e45b0423476051ecbb
Anusha.Haz <63346348+AnushaHazarika@users.noreply.github.com>
2021-03-26 04:47:21 +0530

    'EventsPage'

---
+++ public/js/event/event.js
@@ -0,0 +1,21 @@
+var downArrow = document.getElementById("btn1");
+var upArrow = document.getElementById("btn2");
+var online = document.getElementById("online_event");
+var offline = document.getElementById("offline_event");
+var day1 = document.getElementById("day1");
+var day2 = document.getElementById("day2");
+var toggle = document.getElementById("switch");
+downArrow.onclick = function () {
+    'use strict';
+    day1.style = "display: none";
+    day2.style = "display: block";
+    document.getElementById("main_date").innerText="18";
+};
+
+upArrow.onclick = function () {
+    'use strict';
+    day2.style = "display: none";
+    day1.style = "display: block";
+    document.getElementById("main_date").innerText="17";
+};
+
