a3897702f343e6453f52a4ee622978d5cb91b55e
Anusha.Haz <63346348+AnushaHazarika@users.noreply.github.com>
2021-03-29 11:44:58 +0530

    toggle feature added

--- public/js/event/event.js
+++ public/js/event/event.js
@@ -1,11 +1,8 @@
 
 var downArrow = document.getElementById("btn1");
 var upArrow = document.getElementById("btn2");
-var online = document.getElementById("online_event");
-var offline = document.getElementById("offline_event");
 var day1 = document.getElementById("day1");
 var day2 = document.getElementById("day2");
-var toggle = document.getElementById("switch");
 
 downArrow.onclick = function () {
     'use strict';
@@ -18,4 +15,39 @@ upArrow.onclick = function () {
     day2.style = "display: none";
     day1.style = "display: block";
 };
+// $(document).ready(function(){
+//     $(".slide-left").click(function () {
+//         $('#offline_event').toggle('slide', {direction: "left"}, 1000);
+//     });
+// });
+// $(document).ready(function(){
+//     $(".slide-right").click(function () {
+//         $('#online_event').toggle('slide', {direction: "right"}, 1000);
+//     });
+// });
+
+$(document).ready(function(){
+$( ".slide-left" ).click(function() {
+	// $( "#offline_event" ).animate({width:"0" }, 1000);
+	// $( "#online_event" ).animate({width:"100%" }, 1000);
+        // $('#offline_event').removeClass("moveRight").addClass("moveLeft");
+        // $('#online_event').removeClass("moveRight").addClass("moveLeft");
+        $('.innerLiner').addClass("moveLeft");
+
+	// $( ".innerLiner" ).animate({backgroundPositionX:"-100%"}, 1000);
+	$( ".toggle-cover" ).animate({left:"50%"}, 300);
+});
+
+$( ".slide-right" ).click(function() {
+        // $('#online_event').toggle('slide', {direction: "right"}, 1000);
+        $('.innerLiner').removeClass("moveLeft");
+
+	// $( "#online_event" ).animate({width:"0" }, 1000);
+	// $( "#offline_event" ).animate({width:"100%" }, 1000);
+	$( ".toggle-cover" ).animate({left:"-1.5%"}, 300);
+});
+
+});
+
+
 
